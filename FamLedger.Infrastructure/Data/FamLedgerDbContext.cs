2154ddd88cd75a4aa010fdda886e37b57c6fd5d7
Vikyy98 <wvignesh0698@gmail.com>
2025-11-05 08:40:35 +0530

    Inital Ef setup

---
+++ FamLedger.Infrastructure/Data/FamLedgerDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FamLedger.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamLedger.Infrastructure.Data
+{
+    public class FamLedgerDbContext : DbContext
+    {
+        public FamLedgerDbContext(DbContextOptions<FamLedgerDbContext> options) : base(options)
+        {
+           
+        }
+
+        public DbSet<User> Users { get; set; } = null!;
+    }
+}
