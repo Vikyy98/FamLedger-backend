6969fe934f9b0c883e453485058fcd33847d1b49
Vigneshwaran <vigneshwaran@Vigneshwarans-MacBook-Pro.local>
2025-12-03 23:43:23 +0530

    Swagger updated

--- FamLedger.Api/Program.cs
+++ FamLedger.Api/Program.cs
@@ -70,11 +70,14 @@ var app = builder.Build();
 
 app.UseCors("AllowFrontend");
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
